9b290fc89c9fb1d637bcee21b3024945094ff8ec
tuyennd191@gmail.com <tuyennd191@gmail.com@d40a2564-5f70-1ea3-635d-1692f94f6ea3>
2015-01-15 17:53:23 +0000

    add dialog

--- SLLDT/src/com/dtcs/slldt/screen/outbox/OutboxScreen.java
+++ SLLDT/src/com/dtcs/slldt/screen/outbox/OutboxScreen.java
@@ -1,30 +1,55 @@
 package com.dtcs.slldt.screen.outbox;
 
+import android.app.Dialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import com.dtcs.slldt.common.UserInfoStoreManager;
-import com.dtcs.slldt.screen.SMSAdapter;
+import com.dtcs.slldt.common.DialogCommons;
+import com.dtcs.slldt.common.DialogCommons.OnDialogClickOkListener;
 import com.dtcs.slldt.screen.base.EContactFragment;
 import com.edu.ebookcontact.R;
 
-public class OutboxScreen extends EContactFragment{
+public class OutboxScreen extends EContactFragment {
 
-private ListView mListView;
-	
+	private ListView mListView;
+	private Dialog dCreateSMS;
+
 	@Override
-	protected View onCreateContentView(LayoutInflater inflater,
-			ViewGroup container) {
-		View v = LayoutInflater.from(getActivity()).inflate(R.layout.screen_outbox, container,false);
+	protected View onCreateContentView(LayoutInflater inflater, ViewGroup container) {
+		View v = LayoutInflater.from(getActivity()).inflate(R.layout.screen_outbox, container, false);
+		v.findViewById(R.id.btnOutSMS).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (dCreateSMS == null) {
+					dCreateSMS = DialogCommons.getDialogSent(getActivity(), 
+							mDialogClickOkListener);
+				}
+				dCreateSMS.show();
+				
+//				DialogCommons.getDialogShowSMS(getActivity(), "test content", mDialogClickOkListener).show();
+			}
+		});
 		mListView = (ListView) v.findViewById(R.id.lv_outbox);
 		init();
 		return v;
 	}
-	
-	private void init(){
-//		mListView.setAdapter(new SMSAdapter(getActivity(), UserInfoStoreManager.getInstance().getListSMS()));
+
+	private OnDialogClickOkListener mDialogClickOkListener = new OnDialogClickOkListener() {
+
+		@Override
+		public void onOkClick(Object obj) {
+			Toast.makeText(getActivity(), "abc:  "+ String.valueOf(obj), 1).show();
+		}
+	};
+
+	private void init() {
+		// mListView.setAdapter(new SMSAdapter(getActivity(),
+		// UserInfoStoreManager.getInstance().getListSMS()));
 	}
 
 	@Override
