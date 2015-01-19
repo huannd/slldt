a5634894fa003dde6f1100c32039f4c4943e004d
tuyennd191@gmail.com <tuyennd191@gmail.com@d40a2564-5f70-1ea3-635d-1692f94f6ea3>
2015-01-19 15:25:00 +0000

    add trof chuyeenj

--- trunk/SLLDT/src/com/dtcs/slldt/screen/outbox/OutboxScreen.java
+++ trunk/SLLDT/src/com/dtcs/slldt/screen/outbox/OutboxScreen.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,20 +11,23 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.Toast;
 
 import com.dtcs.slldt.common.DialogCommons;
-import com.dtcs.slldt.common.UserInfoStoreManager;
 import com.dtcs.slldt.common.DialogCommons.OnDialogClickOkListener;
+import com.dtcs.slldt.common.UserInfoStoreManager;
 import com.dtcs.slldt.model.ResultModel;
 import com.dtcs.slldt.model.SMSModel;
 import com.dtcs.slldt.screen.SMSAdapter;
 import com.dtcs.slldt.screen.base.EContactFragment;
 import com.dtcs.slldt.webservice.SMSGatewayWebservice;
 import com.dtcs.slldt.webservice.SMSGatewayWebservice.WebserviceTaskListener;
+import com.dtcs.slldt.widget.SegmentedGroup;
 import com.edu.ebookcontact.R;
 
-public class OutboxScreen extends EContactFragment {
+public class OutboxScreen extends EContactFragment implements OnCheckedChangeListener {
 
 	private ListView mListView;
 	private Dialog dCreateSMS;
@@ -35,27 +37,29 @@ public class OutboxScreen extends EContactFragment {
 	private ArrayList<SMSModel> mDataSendeds;
 	private ArrayList<SMSModel> mDataReceiveds;
 	private ChatType currentChatType = ChatType.CHAT_ALL;
-	
-	public enum ChatType{
-		CHAT_ALL,
-		CHAT_SEND,
-		CHAT_RECEIVE
+	private SegmentedGroup vSegmentedGroup;
+
+	public enum ChatType {
+		CHAT_ALL, CHAT_SEND, CHAT_RECEIVE
 	}
-	
+
 	@Override
 	protected View onCreateContentView(LayoutInflater inflater, ViewGroup container) {
 		View v = LayoutInflater.from(getActivity()).inflate(R.layout.screen_outbox, container, false);
+
+		vSegmentedGroup = (SegmentedGroup) v.findViewById(R.id.segmentedGroup);
+		vSegmentedGroup.setOnCheckedChangeListener(this);
 		v.findViewById(R.id.btnOutSMS).setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				if (dCreateSMS == null) {
-					dCreateSMS = DialogCommons.getDialogSent(getActivity(), 
-							mDialogClickOkListener);
+					dCreateSMS = DialogCommons.getDialogSent(getActivity(), mDialogClickOkListener);
 				}
 				dCreateSMS.show();
-				
-//				DialogCommons.getDialogShowSMS(getActivity(), "test content", mDialogClickOkListener).show();
+
+				// DialogCommons.getDialogShowSMS(getActivity(), "test content",
+				// mDialogClickOkListener).show();
 			}
 		});
 		mListView = (ListView) v.findViewById(R.id.lv_outbox);
@@ -68,17 +72,18 @@ public class OutboxScreen extends EContactFragment {
 		@Override
 		public void onOkClick(Object obj) {
 			if (obj instanceof SMSModel) {
-				SMSModel model = ((SMSModel)obj);
+				SMSModel model = ((SMSModel) obj);
 				showLoading("Đang gửi tin...");
 				SMSGatewayWebservice.sendSMS(model.SDT_Nhan, model.Noi_Dung, new WebserviceTaskListener<ResultModel>() {
-					
+
 					@Override
 					public void onTaskComplete(ResultModel ob, ResultModel result) {
 						hideLoading();
-						if (result!=null && result.getErrorCode() == 0) {
+						if (result != null && result.getErrorCode() == 0) {
 							Toast.makeText(getActivity(), "Gửi tin nhắn thành công.", Toast.LENGTH_SHORT).show();
-						}else{
-							Toast.makeText(getActivity(), "Gửi tin thất bại.Vui lòng gửi lại sau", Toast.LENGTH_SHORT).show();
+						} else {
+							Toast.makeText(getActivity(), "Gửi tin thất bại.Vui lòng gửi lại sau", Toast.LENGTH_SHORT)
+									.show();
 						}
 					}
 				});
@@ -86,7 +91,7 @@ public class OutboxScreen extends EContactFragment {
 		}
 	};
 
-	private void init(){
+	private void init() {
 		mDatas = new ArrayList<SMSModel>();
 		mDataAlls = new ArrayList<SMSModel>();
 		mDataReceiveds = new ArrayList<SMSModel>();
@@ -96,17 +101,17 @@ public class OutboxScreen extends EContactFragment {
 		mListView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> parent,
-					View view, int position, long id) {
-				Dialog dialogSMSContent = DialogCommons.getDialogShowSMS(getActivity(), mDatas.get(position).Noi_Dung, null);
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Dialog dialogSMSContent = DialogCommons.getDialogShowSMS(getActivity(), mDatas.get(position).Noi_Dung,
+						null);
 				dialogSMSContent.show();
 			}
 		});
 		sync();
 	}
 
-	private void filterDatas(ArrayList<SMSModel> datas){
-		if (datas.size()>0) {
+	private void filterDatas(ArrayList<SMSModel> datas) {
+		if (datas.size() > 0) {
 			mDataAlls.clear();
 			mDataReceiveds.clear();
 			mDataSendeds.clear();
@@ -122,8 +127,8 @@ public class OutboxScreen extends EContactFragment {
 			}
 		}
 	}
-	
-	private void switchChat(){
+
+	private void switchChat() {
 		mDatas.clear();
 		switch (currentChatType) {
 		case CHAT_ALL:
@@ -141,15 +146,15 @@ public class OutboxScreen extends EContactFragment {
 		}
 		mAdapter.notifyDataSetChanged();
 	}
-	
+
 	@Override
 	public void sync() {
 		showLoading();
 		SMSGatewayWebservice.getListSmsSended(new WebserviceTaskListener<ArrayList<SMSModel>>() {
-			
+
 			@Override
 			public void onTaskComplete(ArrayList<SMSModel> ob, ResultModel result) {
-				if (ob!=null) {
+				if (ob != null) {
 					filterDatas(ob);
 					switchChat();
 				}
@@ -157,25 +162,44 @@ public class OutboxScreen extends EContactFragment {
 			}
 		});
 	}
-	
+
 	@Override
 	public String getTitle() {
 		return getResources().getString(R.string.lbl_sms_outbox);
 	}
-	
-	class OutBoxAdapter extends SMSAdapter{
+
+	class OutBoxAdapter extends SMSAdapter {
 
 		public OutBoxAdapter(Activity activity, ArrayList<SMSModel> datas) {
 			super(activity, datas);
 		}
-		
+
 		@Override
 		protected String getPhoneAtPosition(int pos) {
 			SMSModel model = getItem(pos);
 			if (model.SDT_Nhan != null && !model.SDT_Nhan.trim().equals("")) {
-				return "To: "+model.SDT_Nhan;
+				return "To: " + model.SDT_Nhan;
 			}
 			return "";
 		}
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		switch (checkedId) {
+		case R.id.rdAll:
+			currentChatType = ChatType.CHAT_ALL;
+			break;
+		case R.id.rdInbox:
+			currentChatType = ChatType.CHAT_RECEIVE;
+			break;
+		case R.id.rdOutbox:
+			currentChatType = ChatType.CHAT_SEND;
+			break;
+
+		default:
+			break;
+		}
+		switchChat();
 	}
 }
